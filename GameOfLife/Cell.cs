3a462241d80ef9a438faa6839288e57fca81965f
CFHoldren <CFHoldren@student.fullsail.edu>
2021-07-01 20:16:14 -0700

    Created a cell class

---
+++ GameOfLife/Cell.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    public class Cell
+    {
+        // Fields
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public bool IsAlive { get; private set; }
+
+        // Constructor
+        public Cell(int x, int y, bool isAlive)
+        {
+            X = x;
+            Y = y;
+            isAlive = IsAlive;
+        }
+        
+    }
+}
